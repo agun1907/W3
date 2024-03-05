9f1540270fd48c166bdd1dddd9da5b9e60e2c448
agun1907 <157813669+agun1907@users.noreply.github.com>
2024-03-05 20:05:55 +0300

    Initial commit

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HelloWorld : MonoBehaviour
+{
+    public int speed = 10;
+    public GameObject textobject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textobject.GetComponent<TextMeshPro>().text = "Hello World";
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed);
+    }
+}
